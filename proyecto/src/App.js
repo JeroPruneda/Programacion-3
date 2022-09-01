60c4ee4be9908ad7132df20cecf6fbfd20e45bad
Jeronimo30 <jsojo@udesa.edu.ar>
2022-09-01 20:08:06 -0300

    hola

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Peliculas from './components/Peliculas/Peliculas'
+import Search from './components/Search/Search'
 
 function App() {
   const opciones = ['Home','Favoritos', 'Ver todas']
   return (
     <React.Fragment>
-      <Header opciones={opciones}/>
+      <Header opciones={opciones} /> <Search />
       <main>
        <Peliculas/>
       </main>
