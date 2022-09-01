58675771aef6fff1f4601432211118471b9132a9
JeroPruneda <89102212+JeroPruneda@users.noreply.github.com>
2022-09-01 15:44:27 -0300

    cambios

--- proyecto/src/components/Peliculas/Peliculas.js
+++ proyecto/src/components/Peliculas/Peliculas.js
@@ -1,12 +1,56 @@
-import React from 'react'
+import React,{Component} from 'react'
+import PeliculasCard from '../PeliculasCard/PeliculasCard';
 
 
-function Peliculas() {
+class Peliculas extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            data: []
+            
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=7a176cc95147be6e695be2faf0e8ff9c')
+        .then(resp => resp.json())
+        .then(data => this.setState({
+            data: data.results
+        }))
+        .catch(err => console.log(err)) 
+    }
+
+
+  render() {
+    return (
+        <>
+        <section className="card-container">
+            {
+                this.state.data.length > 0 ?
+                    this.state.data.map((personaje, idx) => 
+                    <PeliculasCard 
+                    key={personaje + idx} 
+                    name={personaje.title} 
+                    image={personaje.poster_path}
+                    descripcion={personaje.overview}
+                    
+                    />):
+                <h1>Cargando..</h1>
+            }
+      </section>
+        </>
+    )
+  }
+}
+
+
+
+/* function Peliculas() {
   return (
     <>
         <h1>Aca van las pelis </h1> 
         </>//ACA LLAMAMOS A LA API
   )
-}
+} */
 
 export default Peliculas
