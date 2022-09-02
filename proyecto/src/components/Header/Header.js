9a443d13d1547cb48596155964277cd192f7bae9
josesoutou <josesoutou@gmail.com>
2022-09-02 12:34:25 -0300

    buscador e intentando ver mas

--- proyecto/src/components/Header/Header.js
+++ proyecto/src/components/Header/Header.js
@@ -8,9 +8,19 @@ function Header(props){
     <nav>
           <ul className="user">
             <li><img src="./img/logo.png" alt="" /></li>
+            </ul>
+
+            
+            <div className='search-box'>
+            <form action='/search' className='search-form' method='get' target='_top'>
+            <input className='search-text' name='q' placeholder='Escribe lo que buscas' type='text'/>
+            <button className='search-button' type='submit'><span>Busca!</span></button>
+            </form>
+            </div>
+            
             
 
-        </ul>
+
         <ul className="main-nav">
             <li> <Link to ={`/`}>Home</Link></li>
             <li> <Link to ={`/favoritos`}>Favoritos</Link></li>
--- proyecto/src/components/PeliculasCard/PeliculasCard.js
+++ proyecto/src/components/PeliculasCard/PeliculasCard.js
@@ -13,17 +13,17 @@ class PeliculasCard extends Component {
 
     }
   }
-  // verMas(){
-  //   if (this.state.verMas === "show"){
-  //     this.setState 
-  //     ({verMas: "hide"
-  //   })}
-  //   else {
-  //     this.setState({
-  //       verMas :"show"
-  //     })
-  //   }
-  // }
+  verMas(){
+    if(this.state.verMas === 'show'){
+      this.setState({
+        verMas:'hide'
+      })
+    } else {
+      this.setState({
+        verMas:'show'
+      })
+    }
+  }
 
   render(){
     return (
@@ -33,9 +33,9 @@ class PeliculasCard extends Component {
         <article className="polaroid">
             <img className="imagen" src={`https://image.tmdb.org/t/p/w342/${this.props.image}`} alt="" /> 
             <div className="textopolaroid">
-                <p className="textopolaroidtitulo"> {this.props.name}</p>
-              <button onClick = {() => this.verMas ()} > <Link to={`/detalle${this.props.id}`}> VER MAS</Link></button> 
-            
+              <p className="textopolaroidtitulo"> <Link to={`/detalle${this.props.id}`}> {this.props.name}</Link></p>
+              <p className={this.state.verMas}>{this.props.overview}</p> {/* este deberia solo aparecer si tocamos el Ver mas . y overveiw tampoco agarra nose porque*/}
+              <button onClick={()=>this.verMas()}>Ver más</button>
             </div>
         </article>
     </a>
