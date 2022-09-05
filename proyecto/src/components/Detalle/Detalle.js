ae057a9eacfb64c3ce11854627eb0dba60e8129a
Jeronimo30 <jsojo@udesa.edu.ar>
2022-09-05 15:08:51 -0300

    favoritos y vermas

--- proyecto/src/components/Detalle/Detalle.js
+++ proyecto/src/components/Detalle/Detalle.js
@@ -9,7 +9,7 @@ class Detalle extends Component{
         }
     } 
     componentDidMount(){
-        fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}`)
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=7a176cc95147be6e695be2faf0e8ff9c`)
         .then(resp => resp.json())
         .then(data => 
              this.setState
@@ -24,6 +24,8 @@ class Detalle extends Component{
         return(
             <>
             {this.state.listo ? <p>{this.state.detalle.name}</p> : ""}
+
+            <p>hola funciona el detalle</p>
 
             </>
             
--- proyecto/src/components/Home/Home.js
+++ proyecto/src/components/Home/Home.js
@@ -1,31 +1,16 @@
 import React, {Component} from "react";
 import Peliculas from "../Peliculas/Peliculas";
 import Series from "../Series/Series";
-import Header from "../Header/Header";
 
-class Home extends Component{
-    constructor(props){
-        super(props);
-        this.state={
 
-    }
-}
-
-    render(){
-        return(
-            <>
-            <Header />
+function Home() {
+    return (
+        <>
             <Peliculas />
             <Series />
             </>
 
-        )
+    )
 
-    }
-        
-        
-    
-
-    
 }
 export default Home
