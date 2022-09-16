10f2508a28a93b0383dc8edfc043fba24ebe20b5
Jeronimo30 <jsojo@udesa.edu.ar>
2022-09-15 22:42:27 -0300

    buscador ver todas las series

--- proyecto/src/components/TodasSeries/TodasSeries.js
+++ proyecto/src/components/TodasSeries/TodasSeries.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react'
 import SeriesCard from '../SeriesCard/SeriesCard';
-
+import SearchTodas from '../SearchTodas/SearchTodas';
 
 class TodasSeries extends Component {
     constructor(props){
@@ -21,13 +21,34 @@ class TodasSeries extends Component {
         .catch(err => console.log(err)) 
     }
 
+    traerMas(){
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7a176cc95147be6e695be2faf0e8ff9c&language=en-US&page=${this.state.otraPag}`)
+        .then(resp => resp.json())
+        .then(data => this.setState({
+            data: data.results,
+            otraPag: data.page + 1
+        }))
+        .catch(e => console.log(e))
+    }
+
+    filtrarPeli(nombre){
+        let aFiltrado = this.state.data.filter(peli => peli.title.toLowerCase().includes(nombre.toLowerCase()))
+
+        this.setState({
+            data: aFiltrado
+        })
+    } 
+
+
 
   render() {
     return (
         <>
         <div className="palabra">
-    <h3>TODAS LAS SERIES</h3>
-</div>
+            <h3>TODAS LAS SERIES</h3>
+            <SearchTodas filtrar = {(nombre) => this.filtrarPeli(nombre)}/>
+            <button onClick={() => this.traerMas()}> Trae mas peliculas</button>
+        </div>
         <section className="card-container">
             {
                 this.state.data.length > 0 ?
