01f9d7fa4369f51ad2d50242ac289158fc967031
JeroPruneda <89102212+JeroPruneda@users.noreply.github.com>
2022-09-19 18:27:54 -0300

    cambios

--- proyecto/src/components/Detalle/Detalle.js
+++ proyecto/src/components/Detalle/Detalle.js
@@ -5,7 +5,9 @@ class Detalle extends Component{
     constructor(props){
         super(props);
         this.state = {
-            detalle: {},
+            detalle: {
+                genres: []
+            },
 
             
         }
@@ -24,7 +26,32 @@ class Detalle extends Component{
 
     render(){
         return(
-        <>
+            <>
+                {this.state.detalle.genres.length === 0 ?
+                    <img src="https://media4.giphy.com/media/11ASZtb7vdJagM/giphy.gif?cid=ecf05e477jslw010j1zwx5dcdi9ri9lcpp6nzn0aumipgw7n&rid=giphy.gif&ct=g" alt="Cargando..." />
+                    :
+                     <main> 
+                        <div class="portada">
+                             <img className="imgLaptop" src={`https://image.tmdb.org/t/p/w342/${this.state.detalle.poster_path}`} alt=""></img>
+                         </div>
+                         <div className="info">
+                             <h1>{this.state.detalle.title}</h1>                    
+                             <ul className="detalles">
+                                 <li className="fechaDeEstreno">{this.state.detalle.release_date}</li>
+                                 <li className="duracion">{this.state.detalle.runtime} minutes</li>
+                                <p className="genero">{this.state.detalle.genres[0].name}</p>     
+                             </ul>
+                              
+                             <p className="rating">Rating: {this.state.detalle.vote_average}/10</p>
+                             
+                             <p className="sinopsis">{this.state.detalle.overview}</p>
+                             
+                         </div>
+                     </main>
+                       }
+               </> 
+            
+       /*  <>
         
         <main> 
            <div class="portada">
@@ -38,15 +65,15 @@ class Detalle extends Component{
                       
                 </ul>
                 <ul class="genero">{
-/*                                     this.state.detalle.genres.map((Genero) => <li className='genero'>{Genero.name}</li>)
- */                                }</ul> 
+                                    this.state.detalle.genres.map((Genero) => <li className='genero'>{Genero.name}</li>)
+                                }</ul> 
                 <p className="rating">Rating: {this.state.detalle.vote_average}/10</p>
                 <p class="sinopsis"></p>
                 <p className="sinopsis">{this.state.detalle.overview}</p>
                 
             </div>
         </main>
-        </>
+        </> */
         
         )
     } 
